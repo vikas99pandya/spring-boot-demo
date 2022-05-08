c5f0706b3fe20582f19f85257c2554555e531063
v.pandya <v.pandya@assai.nl>
2022-05-08 08:32:39 +0200

    Initial commit

---
+++ src/main/java/com/comitid/test/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.comitid.test.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
